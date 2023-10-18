6300659a2cc7c6ac53c125b7527da64194c08b46
victorgiordani <victor.giordani@protonmail.com>
2023-10-18 13:36:18 +0200

    Done with Breadth-first search

--- main.cpp
+++ main.cpp
@@ -1,151 +1,73 @@
-#include <vector>
-#include <algorithm>
-#include <map>
 #include <iostream>
+#include <vector>
+#include <map>
 #include <string>
+#include <queue>
 
-	std::multimap<std::string, std::string> routesT = {
-	{"DSM", "ORD"},
-	{"ORD", "BGI"},
-	{"BGI", "LGA"},
-	{"SIN", "CDG"},
-	{"CDG", "SIN"},
-	{"CDG", "BUD"},
-	{"DEL", "DOH"},
-	{"DEL", "CDG"},
-	{"TLV", "DEL"},
-	{"EWR", "HND"},
-	{"HND", "ICN"},
-	{"HND", "JFK"},
-	{"ICN", "JFK"},
-	{"JFK", "LGA"},
-	{"EYW", "LHR"},
-	{"LHR", "SFO"},
-	{"SFO", "SAN"},
-	{"SFO", "DSM"},
-	{"SAN", "EYW"}
-};
-	std::multimap<std::string, std::string> switchedRoutesT = {
-    {"ORD", "DSM"},
-    {"BGI", "ORD"},
-    {"LGA", "BGI"},
-    {"CDG", "SIN"},
-    {"SIN", "CDG"},
-    {"BUD", "CDG"},
-    {"DOH", "DEL"},
-    {"CDG", "DEL"},
-    {"DEL", "TLV"},
-    {"HND", "EWR"},
-    {"ICN", "HND"},
-    {"JFK", "HND"},
-    {"JFK", "ICN"},
-    {"LGA", "JFK"},
-    {"LHR", "EYW"},
-    {"SFO", "LHR"},
-    {"SAN", "SFO"},
-    {"DSM", "SFO"},
-    {"EYW", "SAN"}
-};
+std::map<std::string, std::vector<std::string>> convertToSimpleMap(std::multimap<std::string, std::string> routes) {
+    std::map<std::string, std::vector<std::string>> simpleMapList;
+    for (const auto& [key, value] : routes) {
+        simpleMapList[key].push_back(value);
+    }
+    return simpleMapList;
+}
 
+std::vector<std::string> bfs(std::string start, std::string destination, std::map<std::string, std::vector<std::string>>& adjList) {
+    std::queue<std::vector<std::string>> q;
+    q.push({start});
+    
+    while (!q.empty()) {
+        std::vector<std::string> path = q.front();
+        q.pop();
+        std::string current = path.back();
+        
+        if (current == destination) {
+            return path;
+        }
+        
+        for (const auto& next : adjList[current]) {
+            std::vector<std::string> newPath = path;
+            newPath.push_back(next);
+            q.push(newPath);
+        }
+    }
+    
+    return {};
+}
 
-void print_routes(std::vector<std::string> route)
-{
-	for (auto it = route.begin(); it != route.end(); it++)
-	{
-		std::cout << *it;
-		if (it + 1 != route.end())
-			std::cout << " -> ";
-	}
+int main() {
+    std::multimap<std::string, std::string> routesT = {
+        {"DSM", "ORD"},
+        {"ORD", "BGI"},
+        {"BGI", "LGA"},
+        {"SIN", "CDG"},
+        {"CDG", "SIN"},
+        {"CDG", "BUD"},
+        {"DEL", "DOH"},
+        {"DEL", "CDG"},
+        {"TLV", "DEL"},
+        {"EWR", "HND"},
+        {"HND", "ICN"},
+        {"HND", "JFK"},
+        {"ICN", "JFK"},
+        {"JFK", "LGA"},
+        {"EYW", "LHR"},
+        {"LHR", "SFO"},
+        {"SFO", "SAN"},
+        {"SFO", "DSM"},
+        {"SAN", "EYW"}
+    };
+    
+    std::map<std::string, std::vector<std::string>> simpleMapList = convertToSimpleMap(routesT);
+    std::vector<std::string> path = bfs("LHR", "DSM", simpleMapList);
+    
+    for (const auto& airport : path) {
+        std::cout << airport;
+        if (&airport != &path.back()) {
+            std::cout << " -> ";
+        }
+    }
 	std::cout << std::endl;
-}
-
-
-std::vector<std::string> solve(std::string start, std::string target, std::vector<std::string> forward_route = {}, std::vector<std::string> backward_route = {}, int current_depth = 0, std::multimap<std::string, std::string> switchedRoutes = switchedRoutesT, std::multimap<std::string, std::string> routes = routesT)
-{
-
-
-	std::vector<std::string> result;
-	std::multimap<std::string, std::string> croutes;
-	
-	
-	std::cout << "Start: " << start << " , Target: " << target << ", current depth: " << current_depth << std::endl;
-
-	if (current_depth >= routes.size())
-	{
-		std::vector<std::string> noresult = {};
-		return (noresult);
-	}
-
-	if (current_depth % 2)
-		croutes = switchedRoutes;
-	else
-		croutes = routes;
-
-
-	auto range = croutes.equal_range(start);
-	for (auto it = range.first; it != range.second; it++)
-	{
-		//if a path is found
-		if (it->second == target)
-		{
-			std::cout << "Routes found " << std::endl;
-			if (current_depth % 2)
-			{
-				backward_route.push_back(it->first);
-				backward_route.push_back(it->second);
-			}
-			else
-			{
-				forward_route.push_back(it->first);
-				forward_route.push_back(it->second);
-			}
-			
-			result.reserve(forward_route.size() + backward_route.size());
-			result.insert(result.end(), forward_route.begin(), forward_route.end());
-			result.insert(result.end(), backward_route.rbegin(), backward_route.rend());
-			//print_routes(result);
-			print_routes(forward_route);
-			print_routes(backward_route);
-
-			return (result);
-		}
-		else
-		{
-
-			auto s = it->second;	
-			if (current_depth % 2)
-			{
-				backward_route.push_back(start);
-				auto next_it = std::next(it);
-				switchedRoutes.erase(it);
-				it = next_it;
-			}
-			else
-			{
-				forward_route.push_back(start);
-				auto next_it = std::next(it);
-				routes.erase(it);
-				it = next_it;
-			}
-			result = solve(target, s, forward_route, backward_route, current_depth + 1);
-		}
-
-	}
-	return (result);
-}
-
-
-int main()
-{
-	std::vector<std::string> result = solve("HND", "LGA");
-	if (result.empty() == true)
-	{
-		std::cout << "No routes found" << std::endl;
-	}
-	else
-	{
-		print_routes(result);
-	}
-	return 0;
-
+    
+    return 0;
 }
